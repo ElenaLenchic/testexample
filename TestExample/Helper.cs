49348070667a0efab669c681efd6b3a9a6c17341
Elena Shehclkunova <elenaschelkunova@yandex.ru>
2023-05-26 23:26:03 +0300

    program code

---
+++ TestExample/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExample
+{
+    static class Helper
+    {
+        static char[] punctuation = { ',', '.', '?', '!', ':', ';' };
+
+        public static bool ContainsLetters(string line)
+        {
+            return true;
+        }
+
+        public static bool StartsWithLetters(string line)
+        {
+            return false;
+        }
+
+        public static bool EndsWithPunctuation(string line)
+        {
+            return true;
+        }
+
+        public static bool ContainsPunctuationInTheMiddle(string line)
+        {
+            return false;
+        }
+
+    }
+}
